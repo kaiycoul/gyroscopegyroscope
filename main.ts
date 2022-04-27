e9911d0605cfa515ee7824656930ea369818be4b
kaiycoul <88100458+kaiycoul@users.noreply.github.com>
2022-04-27 16:01:14 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,18 @@
+input.onSound(DetectedSound.Loud, function () {
+	
+})
+input.onButtonPressed(Button.AB, function () {
+	
+})
+input.onPinPressed(TouchPin.P1, function () {
+	
+})
+input.onGesture(Gesture.Shake, function () {
+	
+})
+input.onSound(DetectedSound.Quiet, function () {
+	
+})
+basic.forever(function () {
+	
+})
